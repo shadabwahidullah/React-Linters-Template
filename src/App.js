c816ef3ac3b374e36602662deb83fea70e3ae733
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-28 23:22:21 +0430

    Resolve linters' errors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-        Your app is running successfully
+          Your app is running successfully
         </p>
       </header>
     </div>
